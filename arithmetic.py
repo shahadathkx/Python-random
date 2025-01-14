c74aa9b8167ba0c1ee29683aabad0f9938ec78f4
MD SHAHADAT HOSSAIN KHAN <shahadathkx@gmail.com>
2025-01-14 10:40:24 +0600

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,45 @@
+#arithmetic operators
+a = 5
+b = 3
+
+print(a+b)
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+
+print(a%b) #remainder
+
+print(a**b)#a^b
+
+#relational operators
+
+a = 50
+b = 30
+print(a == b) #false
+print(a != b) #true
+print(a >= b) #true
+#assignment operators
+num = 10
+num = num+30
+num += 10
+num -= 10
+num *= 10
+num **= 2
+print("num:",num)
+
+#logical operators
+print(not False)
+print(not True)
+a= 50
+b = 30
+print(not(a>b))
+val1 = True
+val2 = False
+print("ans operator:",val1 and val2)
+print("ans2 operator:",val1 or val2)
+
+
+
